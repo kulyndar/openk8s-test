7a72622aeef7b50473e77e03bf0e8ba6186a1b58
Daria Kulynychenko <kulyndar@gmail.com>
2021-05-02 19:47:20 +0200

    new yamls, liveness probes

---
+++ random-name/src/main/java/cz/cvut/fel/openk8stest/randomname/controller/HealthController.java
@@ -0,0 +1,16 @@
+package cz.cvut.fel.openk8stest.randomname.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<Void> checkHealth(){
+        return ResponseEntity.ok().build();
+    }
+}
